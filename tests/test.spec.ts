7dbba4f3dc01f13afc56fe66c1c5f3629f062a7f
Mariana Luduena <63405308+marianaluduena@users.noreply.github.com>
2025-06-09 19:16:35 -0300

    Added SignUp object

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,8 +1,27 @@
-import{test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import LandingPage from "../pages/LandingPage";
+import SignUp from "../pages/SignUp";
 
-test('has title', async ({ page }) => {
+let landingPage: LandingPage;
+let signUpPage: SignUp;
+
+test('Find and click register btn', async ({ page }) => {
+
+  landingPage = new LandingPage(page);
+  signUpPage = new SignUp(page);
+
   await page.goto('https://qa.biosafeapp.com');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/BioSafeApp/);
+  await landingPage.btnRegister.click();
+  expect(page.url()).toBe('https://qa.biosafeapp.com/signup');
+
+// Signup page test: Fill the form
+
+await signUpPage.name.fill("Jasmine");
+await signUpPage.email.fill("jasmine@fake.com");
+await signUpPage.passWord.fill("Princ3ss!");
+await signUpPage.confimPassWord.fill("Princ3ss!");
+await signUpPage.signUpBtn.click();
+
+await page.waitForTimeout(5000);
 });
